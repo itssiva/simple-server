4a53871f040183de80d54e58832fae1a2d4a8407
itssiva <sivaponugoti@gmail.com>
2019-10-03 02:26:20 -0400

    create a server and listen on the port 80

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+var path = require("path");
+
+const app = express();
+const port = 80;
+
+app.use(express.static("build"));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+
+app.get("/", function(req, res) {
+  res.send("hello world");
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
